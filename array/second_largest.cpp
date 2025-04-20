bfc2282e4739eaf9cc8fcaa6c0f0d2c046847dad
Namita Singh <112771891+namitaa15@users.noreply.github.com>
2025-04-20 13:03:50 +0530

    Create second_largest.cpp

---
+++ array/second_largest.cpp
@@ -0,0 +1,16 @@
+int findSecondLargest(int arr[], int n) {
+    int largest = INT_MIN;
+    int secondLargest = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    return (secondLargest == INT_MIN) ? -1 : secondLargest; // return -1 if no second largest
+}
