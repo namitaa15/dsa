06b7da7aebf5cb740779b02537b1bd06bb19633f
Namita Singh <112771891+namitaa15@users.noreply.github.com>
2025-04-20 20:54:42 +0530

    linear search

---
+++ array/linear_search.cpp
@@ -0,0 +1,8 @@
+int linearSearch(vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == target) {
+            return i; // return index where found
+        }
+    }
+    return -1; // not found
+}
