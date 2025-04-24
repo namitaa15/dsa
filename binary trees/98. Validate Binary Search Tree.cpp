e2b0960bfa3eb12e9b2d2ed69e93121ae8538336
Namita Singh <112771891+namitaa15@users.noreply.github.com>
2025-04-24 19:24:59 +0530

    leetcode 98. Validate Binary Search Tree.cpp

---
+++ binary trees/98. Validate Binary Search Tree.cpp
@@ -0,0 +1,13 @@
+bool validate(TreeNode* node, long long minVal, long long maxVal) {
+    if (!node) return true;
+
+    if (node->val <= minVal || node->val >= maxVal)
+        return false;
+
+    return validate(node->left, minVal, node->val) &&
+           validate(node->right, node->val, maxVal);
+}
+
+bool isValidBST(TreeNode* root) {
+    return validate(root, LLONG_MIN, LLONG_MAX);
+}
