f0849e5e4b729366d349d2f929423b390082f819
Namita Singh <112771891+namitaa15@users.noreply.github.com>
2025-06-23 18:27:20 +0530

    Create subarray with 0sum.cpp

---
+++ array/gfg/subarray with 0sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool subArrayExists(vector<int>& arr) {
+        unordered_set<int> s; // Store prefix sums
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr[i];
+            if (sum == 0 || s.find(sum) != s.end()) {
+               
+                return true;
+            }
+            s.insert(sum); 
+        }
+        return false; 
+    }
+};
