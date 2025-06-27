0848f80f2d7655b1c3e05b54f61cf5a780d6f7bc
Namita Singh <112771891+namitaa15@users.noreply.github.com>
2025-06-27 14:14:53 +0530

    Update 145. Binary Tree Postorder Traversal.cpp

--- binary trees/145. Binary Tree Postorder Traversal.cpp
+++ binary trees/145. Binary Tree Postorder Traversal.cpp
@@ -16,3 +16,32 @@ public:
         return ans;
     }
 };
+
+using stacks
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top();
+            st.pop();
+            ans.push_back(node->val);   // Store root
+
+            // Pehle left, fir right push karo (so that right pehle aaye output me)
+            if (node->left != NULL)
+                st.push(node->left);
+            if (node->right != NULL)
+                st.push(node->right);
+        }
+
+        // Ab ans me hai Root, Right, Left — hume chahiye Left, Right, Root
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
